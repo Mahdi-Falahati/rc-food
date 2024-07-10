2f032692b6011fac0f85b4359e757f5f097e95c9
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-07-10 11:34:41 -0700

    seperate header to another file and make burger menu

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,33 +1,9 @@
-import Link from "next/link";
+import Header from "../module/Header";
 
 export default function Layout({ children }) {
   return (
     <>
-      <header className="md:container md:mx-auto relative top-0 flex justify-around my-5">
-        <h1 className="text-xl tracking-wide font-bold text-lime-800">
-          RC - Food
-        </h1>
-        <section>
-          <Link
-            href="/menu"
-            className="text-lg tracking-wide font-bold text-teal-800 mx-2"
-          >
-            Menu
-          </Link>
-          <Link
-            href="/categories"
-            className="text-lg tracking-wide font-bold text-teal-900 mx-2"
-          >
-            Categories
-          </Link>
-          <Link
-            href="/categories"
-            className="text-lg tracking-wide font-bold text-teal-900 mx-2"
-          >
-            Discunts
-          </Link>
-        </section>
-      </header>
+      <Header />
       <main className="md:container md:mx-auto min-h-[90vh]">{children}</main>
       <footer className="md:container md:mx-auto relative bottom-0">
         footer
---
+++ components/module/Header.jsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Header() {
+  const [open, setToggle] = useState(false);
+
+  const toggleHandler = () => {
+    setToggle(!open);
+  };
+
+  return (
+    <header className="md:container md:mx-auto items-center relative top-0 flex justify-around my-5">
+      <h1 className="text-xl tracking-wide font-bold text-lime-800">
+        <Link href="/">RC - Food</Link>
+      </h1>
+      <section className="md:inline-block hidden">
+        <Link
+          href="/menu"
+          className="text-lg tracking-wide font-bold text-teal-900 mx-2"
+        >
+          Menu
+        </Link>
+        <Link
+          href="/categories"
+          className="text-lg tracking-wide font-bold text-teal-900 mx-2"
+        >
+          Categories
+        </Link>
+        <Link
+          href="/categories"
+          className="text-lg tracking-wide font-bold text-teal-900 mx-2"
+        >
+          Discunts
+        </Link>
+      </section>
+      <div
+        onClick={toggleHandler}
+        className="flex cursor-pointer h-[20px] flex-col justify-between items-center md:hidden"
+      >
+        <span className="h-1 w-10 bg-teal-900"></span>
+        <span className="h-1 w-16 bg-teal-900"></span>
+        <span className="h-1 w-8 bg-teal-900"></span>
+      </div>
+
+      {open && (
+        <section className="md:hidden absolute left-0 right-0 top-10 bg-white flex flex-col h-[300px] justify-around items-center">
+          <Link
+            href="/menu"
+            className="text-lg tracking-wide font-bold text-teal-900 mx-2"
+          >
+            Menu
+          </Link>
+          <Link
+            href="/categories"
+            className="text-lg tracking-wide font-bold text-teal-900 mx-2"
+          >
+            Categories
+          </Link>
+          <Link
+            href="/categories"
+            className="text-lg tracking-wide font-bold text-teal-900 mx-2"
+          >
+            Discunts
+          </Link>
+        </section>
+      )}
+    </header>
+  );
+}
