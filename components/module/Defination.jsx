df733c22db6a223412d25c6d2af1db9ecdbe5940
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-07-11 05:31:54 -0700

    complete definetion component

---
+++ components/module/Defination.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Defination() {
+  return (
+    <div className="my-20 flex justify-center flex-col items-center">
+      <div className="sm:w-[50%] w-[100%] p-8 rounded-md shadow-md shadow-gray-500">
+        <h3 className="text-xl font-bold mb-5">Who We Are?</h3>
+        <p className="text-gray-700 text-justify text-sm">
+          RC-Food company was founded in 2009 by Garrett Camp and Travis
+          Kalanick.The company began food delivery in August 2014 with the
+          launch of the UberFRESH service in Santa Monica, California. In 2015,
+          the platform was renamed to UberEATS and the ordering software was
+          released as its own application, separate from the app for Uber
+          rides.In 2016, it commenced operations in both London and Paris.
+        </p>
+      </div>
+    </div>
+  );
+}
--- components/template/HomePage.jsx
+++ components/template/HomePage.jsx
@@ -1,11 +1,13 @@
 import Attributes from "../module/Attributes";
 import Banner from "../module/Banner";
+import Defination from "../module/Defination";
 
 export default function HomePage() {
   return (
     <section>
       <Banner />
       <Attributes />
+      <Defination />
     </section>
   );
 }
