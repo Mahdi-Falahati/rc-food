e895f78685e41b2d6adbd55f5b03a83b187858ec
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-07-12 11:51:43 -0700

    get data and send with props to details page component

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -1,5 +1,26 @@
 import DetailsPage from "../../components/template/DetailsPage";
 
-export default function Details() {
-  return <DetailsPage />;
+export default function Details({ data }) {
+  return <DetailsPage data={data} />;
+}
+
+export async function getStaticPaths() {
+  const fetching = await fetch(`http://localhost:4000/data/`);
+  const data = await fetching.json();
+  const paths = data.slice(0, 3).map((item) => ({
+    params: {
+      id: item.id.toString(),
+    },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const fetching = await fetch(`http://localhost:4000/data/${params.id}`);
+  const data = await fetching.json();
+
+  return { props: { data } };
 }
