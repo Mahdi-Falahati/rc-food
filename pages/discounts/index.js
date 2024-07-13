fe064b8feb4020f8726a670977cce9fe5789743c
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-07-13 06:28:20 -0700

    get data from server and filter on its finally send to component page

--- pages/discounts/index.js
+++ pages/discounts/index.js
@@ -1,5 +1,18 @@
-import React from "react";
+import DiscountsPage from "../../components/template/DiscountsPage";
 
-export default function Categories() {
-  return <div></div>;
+export default function Categories({ data }) {
+  return <DiscountsPage data={data} />;
+}
+
+export async function getStaticProps() {
+  const fetching = await fetch("http://localhost:4000/data");
+  const response = await fetching.json();
+  const data = response.filter((item) => item.discount !== 0);
+
+  return {
+    props: {
+      data,
+      revalidate: 2,
+    },
+  };
 }
