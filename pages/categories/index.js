a74d25c1dfa1c5551a787be7a6024993b79378d2
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-07-13 07:14:33 -0700

    get data from server and filter on data finally send to page component

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,3 +1,38 @@
-export default function Categories() {
-  return <div></div>;
+import CategoriesPage from "../../components/template/CategoriesPage";
+
+export default function Categories({ data }) {
+  return <CategoriesPage data={data} />;
+}
+
+export async function getServerSideProps(context) {
+  const {
+    query: { difficulty, time },
+  } = context;
+
+  const fetching = await fetch(`http://localhost:4000/data/`);
+  const data = await res.json();
+
+  const filteredData = data.filter((item) => {
+    const difficultyResult = item.details.filter(
+      (detail) => detail.Difficulty && detail.Difficulty === difficulty
+    );
+    const timeResult = item.details.filter((detail) => {
+      const cookingTime = detail["Cooking Time"] || "";
+      const [timeDetail] = cookingTime.split(" ");
+      if (time === "less" && timeDetail && +timeDetail <= 30) {
+        return detail;
+      } else if (time === "more" && +timeDetail > 30) {
+        return detail;
+      }
+    });
+    if (time && difficulty && timeResult.length && difficultyResult.length) {
+      return item;
+    } else if (!time && difficulty && difficultyResult.length) {
+      return item;
+    } else if (time && !difficulty && timeResult.length) {
+      return item;
+    }
+  });
+
+  return { props: { data: filteredData } };
 }
